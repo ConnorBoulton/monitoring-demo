7fb10bdf9824a34263b3935a0a0c63ed1d4059d6
Connor Boulton <cjboulton22@gmail.com>
2021-10-26 11:34:01 -0600

    ready

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const path = require('path')
+const Rollbar = require('rollbar')
+
+let rollbar = new Rollbar({
+    accessToken: '4ab194e1f341492ab8bd1eabdef1f90f',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
+
+
 
 const app = express()
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file great success')
 })
 
 const port = process.env.PORT || 4545
